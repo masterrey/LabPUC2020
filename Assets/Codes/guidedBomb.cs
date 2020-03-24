6d14e318312d5a580628aba71b92e8cd2dd3e5b5
Reinaldo Ramos <masterrey@gmail.com>
2020-03-24 08:16:32 -0300

    Initial commit

---
+++ Assets/Codes/guidedBomb.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guidedBomb : MonoBehaviour
+{
+    public GameObject target;
+
+    Rigidbody rdb;
+    public float bombForce = 1000;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rdb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        transform.LookAt(target.transform);
+        rdb.AddForce(transform.forward*50);
+
+        if (Vector3.Distance(transform.position, target.transform.position) < 1)
+        {
+            Explode();
+        }
+
+    }
+
+
+    void Explode()
+    {
+        print("Boom!");
+        Destroy(gameObject);
+        RaycastHit[] hits;
+        hits = Physics.SphereCastAll(transform.position, 5, Vector3.up, 10);
+
+        if (hits.Length > 0)
+        {
+            foreach (RaycastHit hit in hits)
+            {
+                if (hit.rigidbody)
+                    hit.rigidbody.AddExplosionForce(bombForce, transform.position, 10);
+            }
+        }
+    }
+}
