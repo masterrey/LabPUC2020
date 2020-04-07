0098ee527293c21c54f43e5b92a696727d045b44
Reinaldo Ramos <masterrey@gmail.com>
2020-04-07 08:30:19 -0300

    ia agent

---
+++ Assets/Codes/IAWalk.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IAWalk : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.transform.position);
+    }
+}
