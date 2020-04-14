ce593efc7626d2b834b571612172355fa4cf8c2e
Reinaldo Ramos <masterrey@gmail.com>
2020-04-14 08:57:05 -0300

    PErsonagem trdperson

---
+++ Assets/Codes/TrdWalk.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrdWalk : MonoBehaviour
+{
+    public enum States
+    {
+        idle,
+        walk,
+        jump,
+        die,
+    }
+    public States state;
+    public Animator anim;
+    public Rigidbody rdb;
+
+    public Vector3 move { get; private set; }
+    public float movforce=100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Idle());
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //criacao de vetor de movimento local
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rdb.AddRelativeForce(move * movforce);
+    }
+
+    IEnumerator Idle()
+    {
+        //equivalente ao Start 
+        state = States.idle;
+
+        
+        //
+        while (state == States.idle)
+        {
+            //equivalente ao update
+
+            anim.SetFloat("Velocity", 0);
+            if (rdb.velocity.magnitude > 0.1f)
+            {
+                StartCoroutine(Walk());
+            }
+            //
+            yield return new WaitForEndOfFrame();
+        }
+        //saida do estado
+    }
+
+    IEnumerator Walk()
+    {
+        //equivalente ao Start 
+        state = States.walk;
+
+
+        //
+        while (state == States.walk)
+        {
+            //equivalente ao update
+
+            anim.SetFloat("Velocity", rdb.velocity.magnitude);
+            if (rdb.velocity.magnitude < 0.1f)
+            {
+                StartCoroutine(Idle());
+            }
+            //
+            yield return new WaitForEndOfFrame();
+        }
+        //saida do estado
+    }
+}
